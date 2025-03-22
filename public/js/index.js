3cdc3b141ab6cfe94a555453462093518d262d11
Olive <brunnaroddriguess@gmail.com>
2025-03-21 22:09:33 -0300

    feat - estudo de projeto nikel

---
+++ public/js/index.js
@@ -0,0 +1,91 @@
+const myModal = new bootstrap.Modal("#register-modal");
+let logged = sessionStorage.getItem("logged");
+const session = localStorage.getItem("session");
+
+checkLogged();
+
+//LOGAR NO SISTEMA
+document.getElementById("login-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const email = document.getElementById("email-create-input").value;
+    const password = document.getElementById("password-create-input").value;
+    const checkSession = document.getElementById("password").value;
+
+    const account = getAccount(email);
+
+    if (!account) {
+        alert("Usuário não encontrado.");
+        return;
+    }
+
+    if (account.password !== password) {
+        alert("Senha inválida.");
+        return;
+    }
+
+    saveSessions(email, checkSession);
+    window.location.href = "home.html";
+});
+
+// CRIAR CONTA
+document.getElementById("create-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const email = document.getElementById("email-create-input").value;
+    const password = document.getElementById("password-create-input").value;
+
+    if (email.length < 5) {
+        alert("Preencha o campo com um e-mail válido.");
+        return;
+    }
+
+    if (password.length < 4) {
+        alert("Preencha a senha com no mínimo 4 dígitos.");
+        return;
+    }
+
+    saveAccount({
+        login: email,
+        password: password,
+        transactions: []
+    });
+
+    myModal.hide();
+
+    alert("Conta criada com sucesso!");
+});
+
+function checkLogged() {
+    if (session) {
+        sessionStorage.setItem("logged", session);
+        logged = session;
+    }
+
+    if (logged) {
+        saveSessions(logged, session);
+        window.location.href = "home.html";
+    }
+}
+
+function saveAccount(data) {
+    localStorage.setItem(data.login, JSON.stringify(data));
+}
+
+function saveSessions(data, saveSession) {
+    if (saveSession) {
+        localStorage.setItem("session", data);
+    }
+
+    sessionStorage.setItem("logged", data);
+}
+
+function getAccount(key) {
+    const account = localStorage.getItem(key);
+
+    if (account) {
+        return JSON.parse(account);
+    }
+
+    return "";
+}
